68be8120b9f6b5df831a701da3f89f1bca58a88c
unknown <Administrator@.(none)>
2014-06-18 12:07:15 +0700

    First design, Just layout

---
+++ file/footer.php
@@ -0,0 +1,20 @@
+	</div>
+	<div class="footer-wrapper">
+		<div class="footer">
+			<div class="content">
+				content 1
+			</div>
+			<div class="content">
+				content 2
+			</div>
+			<div class="content">
+				content 3
+			</div>
+			<div class="copy-right">
+				AmeriPros Import Export Co.,Ltd &copy; <?php echo date('Y'); ?>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ file/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<head>
+	<title>AmeriPros</title>
+	<link rel="stylesheet" href="../css/general.css" />
+</head>
+<body>
+<div class="wrapper">
+	<div class="content">
+		<div class="language">
+			<img src="../img/static/kh_flag.png" />
+			<img src="../img/static/us_flag.png" />
+		</div><br/>
+		<div class="top-link">
+			<a href="about_us.php">About us</a> |
+			<a href="about_us.php">Contact us</a> | 
+			<a href="about_us.php">FAQ</a> 
+			<input type="text" placeholder="Search" />
+			<button>Search</button>
+		</div>
+		<div class="menu">
+			<ul class="top-menu">
+				<li><a href="home.php">Home</a></li>
+				<li><a href="product.php">Products</a></li>
+				<li><a href="partner.php">Partner</a></li>
+				<li><a href="customer.php">Customers</a></li>
+				<li><a href="promotion.php">Promotions</a></li>
+				<li><a style="border-right: none" href="order.php">Order</a></li>
+			</ul>
+		</div><br/><br/><br/>
+		<div class="main-content">
+			sdfsdfd
+			
+		</div>
+		
+	
